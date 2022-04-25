361aa31faf892a5eee3285f7117dc453b0050cb0
Marius <marius@example.com>
2022-04-25 16:16:12 +0300

    init

---
+++ js/data.js
@@ -0,0 +1,15 @@
+// inicijuoti ts projekta tsc --init
+// prirasyti tipus
+
+const numbers = [1, 15, 9, -9, -8, -7, 2, 8, 51, 6, 5];
+const numbers2 = [10, 105, 90, -90, -18, -17, 20, 18, 51, 16, 35];
+
+// 1. parasyti funkcija kuri ima masyva kaip argumenta ir grazina vidurki (jei galit su reduce)
+
+// 2. sukurti kinmanji positives ir ikelti i ji visas teigiamas reikmes is numbers2
+
+// 3. parasyti funkcija kuri ima masyva kaip argumenta ir grazins masyvu objekta su [{value: 1}, {value: 15}...]
+
+// 4. parasyti funkcija kuri ima masyva kaip argumenta ir grazina pirmus 4 elementus
+
+//
