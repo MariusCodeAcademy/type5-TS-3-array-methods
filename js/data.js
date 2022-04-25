54d528d653f1561ae7d6231ef7ddb52164e20345
Marius <marius@example.com>
2022-04-25 16:39:00 +0300

    uzd js

--- js/data.js
+++ js/data.js
@@ -12,4 +12,22 @@ const numbers2 = [10, 105, 90, -90, -18, -17, 20, 18, 51, 16, 35];
 
 // 4. parasyti funkcija kuri ima masyva kaip argumenta ir grazina pirmus 4 elementus
 
-//
+// 4.a ta pati funkcija bet su string argumentu. jei argumentas === 'last' grazinam paskutinius 4 elementus, jei nera argumento pirmus 4.
+
+// 5 kuriam funkcija kuri priima 2 argumentus. msg ir dest.
+// msg yra string zinute, o dest yra id elemento i kur padeti msg zinute.
+
+// sunkesnes =========================
+
+// naudojam data.js items masyva
+// s1. isikelti i ts faila ir parasyti kokio tipo duomenys yra items masyve ir pats masyvas
+
+// -- kuriam funkcijas kurios masyva gauna kaip argumenta --
+
+// s2. parasysti funkcija kuri grazina objektu masyva su pavadinimu ir kaina {price: 45, name: 'shoues'}
+
+// s3. parasyti funkcija kuri grazina kainu vidurki
+
+// s4. parasyti funkcija kuri grazina visas kategorijas masyve.
+
+// s5.parasyti funkcija kuri grazina ir iskonsolina bendra prekiu kieki
